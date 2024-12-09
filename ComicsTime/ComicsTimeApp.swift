615f0f8bb743bc0ba81f800978a2c257e5ca2112
Simone Di Blasi <diblasisimone925@gmail.com>
2024-12-09 12:40:53 +0100

    Initial Commit

---
+++ ComicsTime/ComicsTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicsTimeApp.swift
+//  ComicsTime
+//
+//  Created by Simone Di Blasi on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ComicsTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
